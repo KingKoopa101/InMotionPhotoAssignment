1f899eb95f1a2f7216ae33df7ff98aeb22e3567e
Alberto Lopez <alberto20917@gmail.com>
2016-11-28 17:57:44 -0600

    added loading of Photo data using Alamofire.

--- PhotoViewer/PhotoViewer/FirstViewController.swift
+++ PhotoViewer/PhotoViewer/FirstViewController.swift
@@ -6,12 +6,20 @@
 //  Copyright © 2016 Alberto Lopez. All rights reserved.
 //
 
+//http://jsonplaceholder.typicode.com/photos
+
 import UIKit
+import Alamofire
+import AlamofireObjectMapper
+import ObjectMapper
 
 class FirstViewController: UIViewController {
+    
+    var photos : [Photo]  = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,8 +28,55 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        getPhotos()
+    }
     
-
-
+    func getPhotos (){
+        NetworkManager.sharedInstance.requestPhotos{ photos in
+            
+            print(photos)
+            //guard
+            self.photos = photos!
+        }
+    }
 }
 
+class Photo : Mappable {
+    
+    var albumId: String?
+    var id: String?
+    var thumbnailUrl: String?
+    var title: String?
+    var url: String?
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        albumId    <- map["albumId"]
+        id         <- map["id"]
+        thumbnailUrl      <- map["thumbnailUrl"]
+        title       <- map["title"]
+        url  <- map["url"]
+    }
+}
+
+class NetworkManager {
+    
+    static let sharedInstance = NetworkManager()
+    
+    let url = "http://jsonplaceholder.typicode.com/photos"
+    
+    func requestPhotos (completion: @escaping ([Photo]?) -> Void) {
+        
+        Alamofire.request(url).responseArray {(response: DataResponse<[Photo]>) in
+            
+            //test
+            let photoArray:[Photo]? = response.result.value
+            
+            completion(photoArray)
+        }
+    }
+}
